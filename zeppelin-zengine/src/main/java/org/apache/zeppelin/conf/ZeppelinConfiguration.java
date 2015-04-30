langley/zeppelin

Tajo interpreter impelementation (thriftserver version)

zeppelin-zengine/src/main/java/com/nflabs/zeppelin/conf/ZeppelinConfiguration.java
@@ -357,7 +357,8 @@ public class ZeppelinConfiguration extends XMLConfiguration {
         + "com.nflabs.zeppelin.spark.SparkSqlInterpreter,"
         + "com.nflabs.zeppelin.spark.DepInterpreter,"
         + "com.nflabs.zeppelin.markdown.Markdown,"
-        + "com.nflabs.zeppelin.shell.ShellInterpreter"),
+        + "com.nflabs.zeppelin.shell.ShellInterpreter,"
+        + "com.nflabs.zeppelin.tajo.TajoInterpreter"),
         ZEPPELIN_INTERPRETER_DIR("zeppelin.interpreter.dir", "interpreter"),
         ZEPPELIN_ENCODING("zeppelin.encoding", "UTF-8"),
         ZEPPELIN_NOTEBOOK_DIR("zeppelin.notebook.dir", "notebook"),

khalidhuseynov/incubator-zeppelin

zeppelin-zengine/src/main/java/org/apache/zeppelin/conf/ZeppelinConfiguration.java
@@ -375,7 +375,8 @@ public class ZeppelinConfiguration extends XMLConfiguration {
         + "org.apache.zeppelin.spark.SparkSqlInterpreter,"
         + "org.apache.zeppelin.spark.DepInterpreter,"
         + "org.apache.zeppelin.markdown.Markdown,"
-        + "org.apache.zeppelin.shell.ShellInterpreter"),
+        + "org.apache.zeppelin.shell.ShellInterpreter,"
+	  "org.apache.zeppelin.tajo.TajoInterpreter"),
         ZEPPELIN_INTERPRETER_DIR("zeppelin.interpreter.dir", "interpreter"),
         ZEPPELIN_ENCODING("zeppelin.encoding", "UTF-8"),
         ZEPPELIN_NOTEBOOK_DIR("zeppelin.notebook.dir", "notebook"),