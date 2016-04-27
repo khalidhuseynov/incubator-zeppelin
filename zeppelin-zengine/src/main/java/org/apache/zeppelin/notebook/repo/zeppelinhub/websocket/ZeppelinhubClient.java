0d7570d384e9c84188a6cdd4cde4c8f6dbb7ab5e
Anthony Corbacho <corbacho.anthony@gmail.com>
2016-04-27 18:04:07 +0900

    Rename ZeppelinhubWebsocketEvent to ZeppelinhubWebsocket and add dead event when zeppelin close

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/repo/zeppelinhub/websocket/ZeppelinhubClient.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/repo/zeppelinhub/websocket/ZeppelinhubClient.java
@@ -3,8 +3,8 @@ package org.apache.zeppelin.notebook.repo.zeppelinhub.websocket;
 
 import java.net.HttpCookie;
 import java.net.URI;
-import java.util.concurrent.Future;
 
+import org.apache.zeppelin.notebook.repo.zeppelinhub.ZeppelinHubRepo;
 import org.apache.zeppelin.notebook.repo.zeppelinhub.websocket.utils.ZeppelinhubUtils;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.WebSocketListener;
@@ -23,25 +23,26 @@ import com.google.common.collect.Lists;
  */
 public class ZeppelinhubClient {
   private static final Logger LOG = LoggerFactory.getLogger(ZeppelinhubClient.class);
+
   private final WebSocketClient client;
   private final URI zeppelinhubWebsocketUrl;
   private final ClientUpgradeRequest conectionRequest;
-  private final ZeppelinhubWebsocketEvent socket;
-  private static final String TOKEN_HEADER = "X-Zeppelin-Token";
+  
+  // Websocket listenner to zeppelinhub.
+  private final ZeppelinhubWebsocket socket;
   private final String zeppelinhubToken;
-  private Session session;
   
   private ZeppelinhubClient(String url, String token) {
     zeppelinhubWebsocketUrl = URI.create(url);
     client = new WebSocketClient();
     conectionRequest = setConnectionrequest(token);
-    socket = new ZeppelinhubWebsocketEvent();
+    socket = new ZeppelinhubWebsocket();
     zeppelinhubToken = token;
   }
   
   private ClientUpgradeRequest setConnectionrequest(String token) {
     ClientUpgradeRequest request = new ClientUpgradeRequest();
-    request.setCookies(Lists.newArrayList(new HttpCookie(TOKEN_HEADER, token)));
+    request.setCookies(Lists.newArrayList(new HttpCookie(ZeppelinHubRepo.TOKEN_HEADER, token)));
     return request;
   }
   
@@ -52,8 +53,7 @@ public class ZeppelinhubClient {
   public void start() {
     try {
       client.start();
-      Future<Session> fut = client.connect(socket, zeppelinhubWebsocketUrl, conectionRequest);
-      session = fut.get();
+      client.connect(socket, zeppelinhubWebsocketUrl, conectionRequest);
     } catch (Exception e) {
       LOG.error("Cannot connect to zeppelinhub via websocket", e);
     }
@@ -62,18 +62,18 @@ public class ZeppelinhubClient {
   public void stop() {
     LOG.info("Stopping Zeppelinhub websocket client");
     try {
+      socket.stop();
       client.stop();
     } catch (Exception e) {
       LOG.error("Cannot stop zeppelinhub websocket client", e);
     }
-    session = null;
   }
   
   /**
    * Zeppelinhub websocket handler.
    */
-  public class ZeppelinhubWebsocketEvent implements WebSocketListener {
-    private Logger LOG = LoggerFactory.getLogger(ZeppelinhubWebsocketEvent.class);
+  public class ZeppelinhubWebsocket implements WebSocketListener {
+    private Logger LOG = LoggerFactory.getLogger(ZeppelinhubWebsocket.class);
     private Session zeppelinHubSession;
 
     @Override
@@ -112,6 +112,13 @@ public class ZeppelinhubClient {
     private void send(String msg) {
       if (isSessionOpen()) {
         zeppelinHubSession.getRemote().sendStringByFuture(msg);
+      }
+    }
+    
+    private void stop() {
+      send(ZeppelinhubUtils.DeadMessage(zeppelinhubToken));
+      if (isSessionOpen()) {
+        zeppelinHubSession.close();
       }
     }
     
