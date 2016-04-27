229625fa3c2c3846876024ab860442af522ed6ff
Anthony Corbacho <corbacho.anthony@gmail.com>
2016-04-27 21:29:46 +0900

    Refactor some routine events

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/repo/zeppelinhub/websocket/scheduler/ZeppelinHubHeartbeat.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/repo/zeppelinhub/websocket/scheduler/ZeppelinHubHeartbeat.java
@@ -1,8 +1,8 @@
 package org.apache.zeppelin.notebook.repo.zeppelinhub.websocket.scheduler;
 
 import org.apache.zeppelin.notebook.repo.zeppelinhub.ZeppelinhubRestApiHandler;
+import org.apache.zeppelin.notebook.repo.zeppelinhub.websocket.ZeppelinhubClient;
 import org.apache.zeppelin.notebook.repo.zeppelinhub.websocket.utils.ZeppelinhubUtils;
-import org.eclipse.jetty.websocket.api.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,21 +12,19 @@ import org.slf4j.LoggerFactory;
  */
 public class ZeppelinHubHeartbeat implements Runnable {
   private static final Logger LOG = LoggerFactory.getLogger(ZeppelinhubRestApiHandler.class);
-  private Session ZeppelinhubSession;
-  private final String token;
+  private ZeppelinhubClient client;
   
-  public static ZeppelinHubHeartbeat newInstance(Session session, String token) {
-    return new ZeppelinHubHeartbeat(session, token);
+  public static ZeppelinHubHeartbeat newInstance(ZeppelinhubClient client) {
+    return new ZeppelinHubHeartbeat(client);
   }
   
-  private ZeppelinHubHeartbeat(Session session, final String token) {
-    ZeppelinhubSession = session;
-    this.token = token;
+  private ZeppelinHubHeartbeat(ZeppelinhubClient client) {
+    this.client = client;
   }
   
   @Override
   public void run() {
     LOG.debug("Sending PING to zeppelinhub");
-    ZeppelinhubSession.getRemote().sendStringByFuture(ZeppelinhubUtils.PingMessage(token));
+    client.send(ZeppelinhubUtils.pingMessage(client.getToken()));
   }  
 }
