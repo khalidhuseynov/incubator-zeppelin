e97467715911cfb39b185bcd5062d61b0e2bd2e9
Anthony Corbacho <corbacho.anthony@gmail.com>
2016-04-27 21:29:13 +0900

    Some light refactor: move websocket listener to a different class and add more methods

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/repo/zeppelinhub/websocket/ZeppelinhubClient.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/repo/zeppelinhub/websocket/ZeppelinhubClient.java
@@ -1,17 +1,19 @@
 package org.apache.zeppelin.notebook.repo.zeppelinhub.websocket;
 
 
+import java.io.IOException;
 import java.net.HttpCookie;
 import java.net.URI;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.zeppelin.notebook.repo.zeppelinhub.ZeppelinHubRepo;
+import org.apache.zeppelin.notebook.repo.zeppelinhub.websocket.listener.ZeppelinhubWebsocket;
 import org.apache.zeppelin.notebook.repo.zeppelinhub.websocket.scheduler.SchedulerService;
 import org.apache.zeppelin.notebook.repo.zeppelinhub.websocket.scheduler.ZeppelinHubHeartbeat;
-import org.apache.zeppelin.notebook.repo.zeppelinhub.websocket.utils.ZeppelinhubUtils;
+import org.apache.zeppelin.notebook.repo.zeppelinhub.websocket.session.ZeppelinhubSession;
 import org.eclipse.jetty.websocket.api.Session;
-import org.eclipse.jetty.websocket.api.WebSocketListener;
 import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
 import org.eclipse.jetty.websocket.client.WebSocketClient;
 import org.slf4j.Logger;
@@ -20,10 +22,7 @@ import org.slf4j.LoggerFactory;
 import com.google.common.collect.Lists;
 
 /**
- * Zeppelinhub websocket client.
- * 
- * This class connect to Zeppelinhub and send/recieve event.
- *
+ * Manage a zeppelinhub websocket connection.
  */
 public class ZeppelinhubClient {
   private static final Logger LOG = LoggerFactory.getLogger(ZeppelinhubClient.class);
@@ -31,9 +30,6 @@ public class ZeppelinhubClient {
   private final WebSocketClient client;
   private final URI zeppelinhubWebsocketUrl;
   private final ClientUpgradeRequest conectionRequest;
-  
-  // Websocket listenner to zeppelinhub.
-  private final ZeppelinhubWebsocket socket;
   private final String zeppelinhubToken;
   
   private static final int MB = 1048576;
@@ -41,14 +37,69 @@ public class ZeppelinhubClient {
   private static final long CONNECTION_IDLE_TIME = TimeUnit.SECONDS.toMillis(30);
   
   private SchedulerService schedulerService;
+  private ZeppelinhubSession zeppelinhubSession;
+  
+  public static ZeppelinhubClient newInstance(String url, String token) {
+    return new ZeppelinhubClient(url, token);
+  }
+  
+  public void start() {
+    try {
+      client.start();
+      zeppelinhubSession = connect();
+      addRoutines();
+    } catch (Exception e) {
+      LOG.error("Cannot connect to zeppelinhub via websocket", e);
+    }
+  }
+  
+  public void stop() {
+    LOG.info("Stopping Zeppelinhub websocket client");
+    try {
+      zeppelinhubSession.close();
+      schedulerService.close();
+      client.stop();
+    } catch (Exception e) {
+      LOG.error("Cannot stop zeppelinhub websocket client", e);
+    }
+  }
+  
+  public String getToken() {
+    return this.zeppelinhubToken;
+  }
+  
+  public void send(String msg) {
+    if (!isConnectedToZeppelinhub()) {
+      LOG.info("Zeppelinhub connection is not open, opening it");
+      zeppelinhubSession = connect();
+    }
+    zeppelinhubSession.sendByFuture(msg);
+  }
+  
+  private boolean isConnectedToZeppelinhub() {
+    return  (zeppelinhubSession == null || !zeppelinhubSession.isSessionOpen()) ? false : true;
+  }
   
   private ZeppelinhubClient(String url, String token) {
     zeppelinhubWebsocketUrl = URI.create(url);
     client = createNewWebsocketClient();
     conectionRequest = setConnectionrequest(token);
-    socket = new ZeppelinhubWebsocket();
     zeppelinhubToken = token;
     schedulerService = SchedulerService.create(10);
+  }
+  
+  private ZeppelinhubSession connect() {
+    ZeppelinhubSession zeppelinSession;
+    try {
+      ZeppelinhubWebsocket ws = ZeppelinhubWebsocket.newInstance(zeppelinhubToken);
+      Future<Session> future = client.connect(ws, zeppelinhubWebsocketUrl, conectionRequest);
+      Session session = future.get();
+      zeppelinSession = ZeppelinhubSession.createInstance(session, zeppelinhubToken);
+    } catch (IOException | InterruptedException | ExecutionException e) {
+      LOG.info("Couldnt connect to zeppelinhub", e);
+      zeppelinSession = ZeppelinhubSession.EMPTY;
+    }
+    return zeppelinSession;
   }
   
   private ClientUpgradeRequest setConnectionrequest(String token) {
@@ -64,87 +115,8 @@ public class ZeppelinhubClient {
     return client;
   }
   
-  private void addRoutines(Session session) {
-    schedulerService.add(ZeppelinHubHeartbeat.newInstance(session, zeppelinhubToken), 10, 23);
+  private void addRoutines() {
+    schedulerService.add(ZeppelinHubHeartbeat.newInstance(this), 10, 23);
   }
   
-  public static ZeppelinhubClient newInstance(String url, String token) {
-    return new ZeppelinhubClient(url, token);
-  }
-  
-  public void start() {
-    try {
-      client.start();
-      Future<Session> future = client.connect(socket, zeppelinhubWebsocketUrl, conectionRequest);
-      Session session = future.get();
-      addRoutines(session);
-    } catch (Exception e) {
-      LOG.error("Cannot connect to zeppelinhub via websocket", e);
-    }
-  }
-  
-  public void stop() {
-    LOG.info("Stopping Zeppelinhub websocket client");
-    try {
-      socket.stop();
-      client.stop();
-    } catch (Exception e) {
-      LOG.error("Cannot stop zeppelinhub websocket client", e);
-    }
-  }
-  
-  /**
-   * Zeppelinhub websocket handler.
-   */
-  public class ZeppelinhubWebsocket implements WebSocketListener {
-    private Logger LOG = LoggerFactory.getLogger(ZeppelinhubWebsocket.class);
-    private Session zeppelinHubSession;
-
-    @Override
-    public void onWebSocketBinary(byte[] payload, int offset, int len) {}
-
-    @Override
-    public void onWebSocketClose(int statusCode, String reason) {
-      LOG.info("Closing websocket connection [{}] : {}", statusCode, reason);
-      this.zeppelinHubSession = null;
-    }
-
-    @Override
-    public void onWebSocketConnect(Session session) {
-      LOG.info("Opening a new session to Zeppelinhub {}", session.hashCode());
-      this.zeppelinHubSession = session;
-      send(ZeppelinhubUtils.liveMessage(zeppelinhubToken));
-    }
-
-    @Override
-    public void onWebSocketError(Throwable cause) {
-      LOG.info("Got error", cause);
-    }
-
-    @Override
-    public void onWebSocketText(String message) {
-      LOG.info("Got msg {}", message);
-      if (isSessionOpen()) {
-        // do something.
-      }
-    }
-
-    private boolean isSessionOpen() {
-      return ((zeppelinHubSession != null) && (zeppelinHubSession.isOpen())) ? true : false;
-    }
-    
-    private void send(String msg) {
-      if (isSessionOpen()) {
-        zeppelinHubSession.getRemote().sendStringByFuture(msg);
-      }
-    }
-    
-    private void stop() {
-      send(ZeppelinhubUtils.DeadMessage(zeppelinhubToken));
-      if (isSessionOpen()) {
-        zeppelinHubSession.close();
-      }
-    }
-    
-  }
 }
